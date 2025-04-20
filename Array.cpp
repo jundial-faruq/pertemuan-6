39131ea13077183844f9e50559f002aac0344388
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-04-20 14:15:05 +0700

    step 3

--- Array.cpp
+++ Array.cpp
@@ -23,3 +23,13 @@ int main(){
     }
     
 }
+
+#include <iostream>
+using namespace std;
+
+//membuat struct mahasiswa
+struct mahasiswa{
+    string nim;
+    string nama;
+    string alamat;
+};
