da35100f42ccd286b44058d530171d031b649a73
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-04-20 14:55:02 +0700

    step 9

--- testing4.cpp
+++ testing4.cpp
@@ -11,3 +11,34 @@ struct mahasiswa{
     string nama;
     detailAlamat alamat;
 };
+
+
+int main(){
+  
+    mahasiswa mhs[2];
+
+    for(int i = 0; i < 2; i++){ 
+    cout << "Mahasiswa ke-" << i+1 << endl;
+    cout << "Masukkan NIM = ";
+    cin >> mhs[i].nim;
+    cin.ignore();
+    cout << "Masukkan Nama = ";
+    getline(cin, mhs[i].nama);
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs[i].alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs[i].alamat.kota;
+    }
+
+    cout << endl;
+  
+    cout << "Data Mahasiswa" << endl;
+    
+    for (int i = 0; i < 2; i++) { 
+    cout << "Data Mahasiswa ke-" << i+1 << endl;
+    cout << "NIM = " << mhs[i].nim << endl;
+    cout << "Nama = " << mhs[i].nama << endl;
+    cout << "Alamat Desa = " << mhs[i].alamat.desa << endl;
+    cout << "Alamat Kota = " << mhs[i].alamat.kota << endl;
+    }
+}
