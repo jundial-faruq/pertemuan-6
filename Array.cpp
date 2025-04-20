a74e3f01df85be4edb4e7ba9aaf042cebe30b2d3
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-04-20 14:40:28 +0700

    step 6

--- Array.cpp
+++ Array.cpp
@@ -24,52 +24,8 @@ int main(){
     
 }
 
-#include <iostream>
-using namespace std;
-
-//membuat struct mahasiswa
-struct mahasiswa{
-    string nim;
-    string nama;
-    string alamat;
-};
-
-int main(){
-    mahasiswa mhs;
-
-    mhs.nim = "2024014001";
-    mhs.nama = "Abra";
-    mhs.alamat = "Kasihan";
-
-    cout << "Masukan NIM = ";
-    cin >> mhs.nim;
-    cout << "Masukan Nama = ";
-    cin >> mhs.nama;
-    cout << "Masukan Alamat = ";
-    cin >> mhs.alamat;
-
-    cout << endl;
-    cout << "Data Mahasiswa" << endl;
-    
-    cout << "NIM = " << mhs.nim << endl;
-    cout << "Nama = " << mhs.nama << endl;
-    cout << "Alamat = " << mhs.alamat << endl;
-
-}
 
 
-#include <iostream>
-using namespace std;
 
-struct detailAlamat{
-    string desa;
-    string kota;
-};
 
-struct mahasiswa{
-    string nim;
-    string nama;
-    detailAlamat alamat
-
-};
 
---
+++ Testing2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+
+};
+
+int main(){
+    mahasiswa mhs;
+
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukan Alamat Kota = ";
+    mhs.alamat.kota;
+
+    cout <<  endl;
+
+    cout << "Data Mahasiswa" << endl;
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat Desa" << mhs.alamat.desa << endl;
+    cout << "Alamat Kota" << mhs.alamat.kota << endl;
+    
+}
