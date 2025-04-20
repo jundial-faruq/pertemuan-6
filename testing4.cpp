18bdaeca34dfc331546a349834dfe2678b33b2d7
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-04-20 14:53:52 +0700

    step 8

---
+++ testing4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
