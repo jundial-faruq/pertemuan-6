7757c6b042b5c3b31fe6b91cddd6c47c6347eaa9
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-04-20 14:51:34 +0700

    step 7

--- testing3.cpp
+++ testing3.cpp
@@ -13,3 +13,26 @@ struct mahasiswa{
     detailAlamat alamat;
 };
 
+int main()
+{
+    mahasiswa mhs;
+
+    cout << "Masukan NIM = ";
+    cin >> mhs.nim;
+    cin.ignore();
+    cout << "Masukan Nama = ";
+    getline(cin, mhs.nama);
+    cout << "Masukan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << endl;
+    cout << "Data Mahasiswa" << endl;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "Nama = " << mhs.nama << endl;
+    cout << "Alamat Desa = " << mhs.alamat.desa << endl; 
+    cout << "Alamat Kota = " << mhs.alamat.kota << endl;
+
+}
